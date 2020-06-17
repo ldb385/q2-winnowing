5b1dc8e0e1689bd4657dc45b29bf93a75e479310
ldb385 <ldb385@usask.ca>
2020-06-17 17:32:32 -0600

    documentation for step6 is much better. reformatting based off y should have equal amount of rows as x has values for 'x' and 'y' should complete the current error hopefully https://stackoverflow.com/questions/43736314/error-in-g-that-non-conformable-arrays

--- q2_winnowing/step6/Step6_Permanova.py
+++ q2_winnowing/step6/Step6_Permanova.py
@@ -7,15 +7,16 @@ import pandas as pd
 import numpy as np
 from rpy2.robjects.packages import importr
 import rpy2.robjects.packages as rpackages
-from rpy2.robjects.vectors import StrVector
+from rpy2.robjects.vectors import StrVector, FloatVector
 from rpy2.robjects import r
+from rpy2.robjects import default_converter, conversion
 from scipy.spatial.distance import pdist,squareform
 from rpy2.robjects import pandas2ri
 from rpy2.robjects import IntVector, Formula
 import rpy2.robjects as robjects
 import matplotlib.pyplot as plt
 from scipy.interpolate import UnivariateSpline
-
+from rpy2.robjects.conversion import localconverter
 
 # Install packages
 pandas2ri.activate()
@@ -43,145 +44,6 @@ rstats = importr('stats')
 rprint = r['print']
 rpar = r['par']
 rplot = r['plot']
-
-# # <><><> DEFINE FUNCTIONS <><><>
-#
-# #for i in range(0,len(brome_dg_auc)):
-# def perform_permanova_original(sample_file,data_file_1,data_file_2,output_file):
-#     print("Processing Input File: "+str(data_file_1))
-#     print("Processing Input file: "+str(data_file_2))
-#     brome_sample = pd.read_csv(sample_file)
-#     brome_dg_auc = pd.read_csv(data_file_1)
-#     brome_dg_auc100 = pd.read_csv(data_file_2)
-#     brome_dg_premanova = pd.DataFrame(columns=['test', 'order', 'auc','SumsOfSqs','MeanSqs','F.model','R2','Pval','N.taxa','F.model.scale'])
-#     for i in range(0,len(brome_dg_auc)):
-#         brome_dg = brome_dg_auc100.iloc[:,range(0,brome_dg_auc.iloc[i,2])]
-#         rbrome_dg = pandas2ri.py2ri(brome_dg)
-#         rbrome_sample = pandas2ri.py2ri(brome_sample["type"])
-#         rbrome_dg_hel = rvegan.vegdist(rvegan.decostand(rbrome_dg, "hellinger"), "euclidean")
-#         rformula = Formula('y ~ x')
-#         renv = rformula.environment
-#         renv['x'] = rbrome_sample
-#         renv['y'] = rbrome_dg_hel
-#         radonis = rvegan.adonis(rformula,permutations=999)
-#         brome_dg_premanova.loc[i] = ["auc"+str(i+1)]+[str(i+1)]+[i+1]+[radonis[0][1][0]]+[radonis[0][2][0]]+[radonis[0][3][0]]+[radonis[0][4][0]]+[radonis[0][5][0]]+[len(brome_dg.columns)]+[-1.0]
-#
-#     brome_dg_premanova["F.model.scale"] = (brome_dg_premanova["F.model"] - min(brome_dg_premanova["F.model"]))/(max(brome_dg_premanova["F.model"]) - min(brome_dg_premanova["F.model"]))
-#     brome_dg_premanova.index = range(1,len(brome_dg_premanova)+1)
-#     brome_dg_premanova.to_csv(output_file)
-#     brome_dg_premanova.loc[0] = ["auc0"]+[0]+[0]+[0]+[0]+[0]+[0]+[0]+[0]+[0]
-#     brome_dg_premanova.sort_index(axis=0,inplace=True)
-#     print("Output is Written in File: "+str(output_file)+"\n\n")
-#     return brome_dg_premanova
-#
-#
-#
-# # <><><> DEFINE EXECUTION FUNCTION <><><>
-# def main_original():
-#     sample_files = ["Brome_BFA_AB_sample_info.csv"]
-#     data_files_1 = ["brome.dg.auc.csv", "brome.cl.auc.csv", "brome.bw.auc.csv", "brome.ei.auc.csv"]
-#     data_files_2 = [
-#         "ADD1_AUC100_MIC0.2_Brome_bacfunarc_dw_otu_table-graph_centrality-degree-selectallbyall-abundances.csv",
-#         "ADD1_AUC100_MIN3_Brome_bacfunarc_dw_otu_table-graph_centrality-closeness-selectallbyall-abundances.csv",
-#         "ADD1_AUC100_MIN3_MIC0.2__Brome_bacfunarc_dw_otu_table-graph_centrality-betweenness-selectallbyall-abundances.csv",
-#         "ADD1_AUC100_MIN3Brome_bacfunarc_dw_otu_table-graph_centrality-eigenvector-selectallbyall-abundances.csv"]
-#     output_files = ["Brome.dg.permanova-new.csv", "Brome.cl.permanova-new.csv", "Brome.bw.permanova-new.csv",
-#                     "Brome.ei.permanova-new.csv"]
-#
-#     for file_index in range(0, len(data_files_1)):
-#         brome_permanova = perform_permanova_original(sample_files[0], data_files_1[file_index], data_files_2[file_index],
-#                                             output_files[file_index])
-#         rf_model_scale = pandas2ri.py2ri(brome_permanova["F.model.scale"])
-#         sliding_sd = rzoo.rollapply(rf_model_scale, width=5, FUN=rstats.sd, fill='NA')
-#         if (file_index == 0):
-#             plt.figure()
-#             plt.axis([0, 100, 0.0, 1.0])
-#             plt.xlabel('AUC%')
-#             plt.ylabel('F-Score(scaled)')
-#             plt.plot(
-#                 brome_permanova.iloc[[x for x in range(39, 45)] + [y for y in range(45, 101, 5)], [2]].values.tolist(),
-#                 brome_permanova.iloc[[x for x in range(39, 45)] + [y for y in range(45, 101, 5)], [9]].values.tolist(),
-#                 'ro', color="black")
-#             plt.plot(brome_permanova.iloc[[x for x in range(0, 36, 5)] + [37, 38, 39], [2]].values.tolist(),
-#                      brome_permanova.iloc[[x for x in range(0, 36, 5)] + [36, 37, 38], [9]].values.tolist(), 'ro',
-#                      color="red")
-#             spl = UnivariateSpline(brome_permanova["auc"].values.tolist(),
-#                                    brome_permanova["F.model.scale"].values.tolist())
-#             xs = np.linspace(0, 100, 10)
-#             plt.plot(xs, spl(xs), 'g', lw=1)
-#             # plt.plot(brome_permanova["auc"].values.tolist(),brome_permanova["F.model.scale"],color="green")
-#             plt.plot([x for x in range(0, 101)], sliding_sd, color="red")
-#             plt.plot(brome_permanova.iloc[36, 2], brome_permanova.iloc[36, 9], 'ro', color="blue")
-#             plt.text(brome_permanova.iloc[36, 2] + 40, brome_permanova.iloc[36, 9],
-#                      str(brome_permanova.iloc[36, 8]) + " OTUs", color="blue")
-#             plt.legend(loc=1, labels="E")
-#
-#         elif (file_index == 1):
-#             plt.figure()
-#             plt.axis([0, 100, 0.0, 1.1])
-#             plt.xlabel('AUC%')
-#             plt.ylabel('F-Score(scaled)')
-#             plt.plot(brome_permanova.iloc[[8, 9, 10] + [x for x in range(11, 101, 5)], [2]].values.tolist(),
-#                      brome_permanova.iloc[[8, 9, 10] + [x for x in range(11, 101, 5)], [9]].values.tolist(), 'ro',
-#                      color="black")
-#             plt.plot(brome_permanova.iloc[[x for x in range(1, 7)], [2]].values.tolist(),
-#                      brome_permanova.iloc[[x for x in range(1, 7)], [9]].values.tolist(), 'ro', color="red")
-#             spl = UnivariateSpline(brome_permanova["auc"].values.tolist(),
-#                                    brome_permanova["F.model.scale"].values.tolist())
-#             xs = np.linspace(0, 101, 10)
-#             plt.plot(xs, spl(xs), 'g', lw=1)
-#             plt.plot([x for x in range(0, 101)], sliding_sd, color="red")
-#             plt.plot(brome_permanova.iloc[4, 2], brome_permanova.iloc[4, 9], 'ro', color="blue")
-#             plt.text(brome_permanova.iloc[4, 2] + 40, brome_permanova.iloc[4, 9],
-#                      str(brome_permanova.iloc[4, 8]) + " OTUs", color="blue")
-#             plt.legend(loc=1, labels="F")
-#
-#         elif (file_index == 2):
-#             plt.figure()
-#             plt.axis([0, 100, 0.0, 1.0])
-#             plt.xlabel('AUC%')
-#             plt.ylabel('F-Score(scaled)')
-#             plt.plot(
-#                 brome_permanova.iloc[[x for x in range(36, 40)] + [y for y in range(40, 101, 5)], [2]].values.tolist(),
-#                 brome_permanova.iloc[[x for x in range(36, 40)] + [y for y in range(40, 101, 5)], [9]].values.tolist(),
-#                 'ro', color="black")
-#             plt.plot(brome_permanova.iloc[[x for x in range(0, 31, 5)] + [32, 33, 34, 35], [2]].values.tolist(),
-#                      brome_permanova.iloc[[x for x in range(0, 31, 5)] + [32, 33, 34, 35], [9]].values.tolist(), 'ro',
-#                      color="red")
-#             spl = UnivariateSpline(brome_permanova["auc"].values.tolist(),
-#                                    brome_permanova["F.model.scale"].values.tolist())
-#             xs = np.linspace(0, 100, 10)
-#             plt.plot(xs, spl(xs), 'g', lw=1)
-#             plt.plot([x for x in range(0, 101)], sliding_sd, color="red")
-#             plt.plot(brome_permanova.iloc[35, 2], brome_permanova.iloc[35, 9], 'ro', color="blue")
-#             plt.text(brome_permanova.iloc[35, 2] + 40, brome_permanova.iloc[35, 9] + 0.05,
-#                      str(brome_permanova.iloc[35, 8]) + " OTUs", color="blue")
-#             plt.legend(loc=1, labels="G")
-#
-#         elif (file_index == 3):
-#             plt.figure()
-#             plt.axis([0, 100, 0.0, 1.0])
-#             plt.xlabel('AUC%')
-#             plt.ylabel('F-Score(scaled)')
-#             plt.plot(
-#                 brome_permanova.iloc[[x for x in range(26, 31)] + [y for y in range(30, 101, 5)], [2]].values.tolist(),
-#                 brome_permanova.iloc[[x for x in range(26, 31)] + [y for y in range(30, 101, 5)], [9]].values.tolist(),
-#                 'ro', color="black")
-#             plt.plot(brome_permanova.iloc[[x for x in range(0, 21, 5)] + [22, 23], [2]].values.tolist(),
-#                      brome_permanova.iloc[[x for x in range(0, 21, 5)] + [22, 23], [9]].values.tolist(), 'ro',
-#                      color="red")
-#             spl = UnivariateSpline(brome_permanova["auc"].values.tolist(),
-#                                    brome_permanova["F.model.scale"].values.tolist())
-#             xs = np.linspace(0, 100, 5)
-#             plt.plot(xs, spl(xs), 'g', lw=1)
-#             plt.plot([x for x in range(0, 101)], sliding_sd, color="red")
-#             plt.plot(brome_permanova.iloc[21, 2], brome_permanova.iloc[21, 9], 'ro', color="blue")
-#             plt.text(brome_permanova.iloc[21, 2] + 40, brome_permanova.iloc[21, 9],
-#                      str(brome_permanova.iloc[21, 8]) + " OTUs", color="blue")
-#             plt.legend(loc=1, labels="H")
-
-
-
 
 
 # <><><> DEFINE FUNCTIONS <><><>
@@ -292,39 +154,68 @@ def perform_permanova_dataFrame( sample_file, data_frame_1, data_frame_2, output
         dump.write( f"Processing Input File: {str(data_frame_1)} \n")
         dump.write( f"Processing Input file: {str(data_frame_2)} \n")
 
-    df_sample = pd.read_csv(sample_file)
+    df_sample = pd.read_csv(sample_file) # AB horizon, BRA
 
-    df_dg_auc = data_frame_1
-    df_dg_auc100 = data_frame_2
+    df_dg_auc = data_frame_1 # AUC curves for subsetting
+    df_dg_auc100 = data_frame_2 # Abundances used for Hellinger
+
+    # This is what our dataframe output should be formatted as, will need to build as each index is iterated
     df_dg_premanova = pd.DataFrame(columns=['test', 'order', 'auc','SumsOfSqs','MeanSqs','F.model','R2','Pval','N.taxa','F.model.scale'])
 
     # Setup variables before loop to avoid extra computation
-    df_data_sample = np.array( df_sample["type"] )
-    df_data_sample = pd.DataFrame({ 'type': df_data_sample }) # Need to reintroduce key inorder to convert
-    rdf_data_sample = pandas2ri.py2rpy( df_data_sample )
+    typeIndex = int( df_sample.columns.get_loc("type") ) # Since Rpy doesn't allow for string index must get column int index
+    with localconverter( default_converter + pandas2ri.converter ):
+        rdf_sample = conversion.py2rpy( df_sample )
+    rdf_data_sample = rdf_sample[ typeIndex ]
 
-    rformula = Formula('y ~ x')
+    rformula = Formula('x ~ y') # Note, y is independent while x is dependant
 
-    for i in range(0,len(df_dg_auc)):
-        df_data_dg = df_dg_auc100.iloc[:,range(0,df_dg_auc.iloc[i,2])]
-        rdf_data_dg = pandas2ri.py2rpy(df_data_dg)
-        rdf_data_dg_hel = rvegan.vegdist(rvegan.decostand(rdf_data_dg, "hellinger"), "euclidean")
+    for i in range( 0, len( df_dg_auc) ): # all auc values are used to compute
 
+        # This is STEP 1
+        df_data_dg = df_dg_auc100.iloc[:, 0:int(df_dg_auc.iloc[i,2]) ] # get all columns with rows 0 to value at [i,2]
+        with localconverter(default_converter + pandas2ri.converter):
+            rdf_data_dg = conversion.py2rpy(df_data_dg)
+
+        # This is STEP 2
+        # Convert to Hellinger distance matrix
+        rdf_data_dg_hel = rvegan.vegdist(rvegan.decostand( rdf_data_dg, "hellinger"), "euclidean")
+
+        # with localconverter( default_converter + pandas2ri.converter):
+        #     pd_from_r_df = conversion.rpy2py(rdf_data_dg_hel)
+        #
+        # count = 0
+        # for x in pd_from_r_df:
+        #     count += 1
+        # print( count )
+
+        print( "Sample", len(rdf_data_sample) )
+        print( "Other", len(rdf_data_dg_hel ) )
+
+        # This is STEP 3
+        # Setup formula for the adonis calculation
         renv = rformula.environment
-        renv['x'] = rdf_data_sample
-        renv['y'] = rdf_data_dg_hel
+        renv['x'] = FloatVector( rdf_data_dg_hel ) # y should have equal amount of rows as x has values
 
-        radonis = rvegan.adonis(rformula,permutations=999)
-        df_dg_premanova.loc[i] = ["auc"+str(i+1)]+[str(i+1)]+[i+1]+[radonis[0][1][0]]+[radonis[0][2][0]]+[radonis[0][3][0]]+[radonis[0][4][0]]+[radonis[0][5][0]]+[len(df_data_dg.columns)]+[-1.0]
+        radonis = rvegan.adonis( rformula, permutations=999)
 
+        # This is STEP 4
+        # Must build the permanova table stated earlier
+        df_dg_premanova.loc[i] = [f"auc{str(i+1)}"] + [f"{i+1}"] + [i+1] + [radonis[0][1][0]] + [radonis[0][2][0]] + \
+                                 [radonis[0][3][0]] + [radonis[0][4][0]] + [radonis[0][5][0]] + [len(df_data_dg.columns)] + [-1.0]
+        # NOTE: this is form
+            # DataFrame[ Index ] = Test, Order, AUC, SumOfSquars, F.Model, R2, Pval, N.taxa, F.Model.scale
+
+    # This is STEP 4.5
+    # Convert F.model to a scaled version of F.model
     df_dg_premanova["F.model.scale"] = (df_dg_premanova["F.model"] - min(df_dg_premanova["F.model"]))/(max(df_dg_premanova["F.model"]) - min(df_dg_premanova["F.model"]))
     df_dg_premanova.index = range(1,len(df_dg_premanova)+1)
 
     if( detailed ):
         df_dg_premanova.to_csv( output_file )
 
-    df_dg_premanova.loc[0] = ["auc0"]+[0]+[0]+[0]+[0]+[0]+[0]+[0]+[0]+[0]
-    df_dg_premanova.sort_index(axis=0,inplace=True)
+    # df_dg_premanova.loc[0] = ["auc0"]+[0]+[0]+[0]+[0]+[0]+[0]+[0]+[0]+[0]
+    # df_dg_premanova.sort_index(axis=0,inplace=True)
 
     if( verbose and detailed ):
         dump.write( f"Output is Written in File: {str(output_file)} \n\n" )
@@ -376,9 +267,9 @@ def main_dataFrame( dataFrame1, dataFrame2, sampleFile,  name, detailed=False, v
 # <><> TEST <><>
 # Testing dataframe function
 test_sample = "./test_data/Brome_BFA_AB_sample_info.csv"
-test_abundance = pd.read_csv("./test_data/bromeA_all-abundances-0.csv")
-test_AUC = pd.read_csv("./test_data/testframe_1_auc_result.csv")
-main_dataFrame( test_abundance, test_AUC, test_sample, "Test_Step6", True, True)
+test_abundance = pd.read_csv("./test_data/ADD1_AUC100_MIC0.2_Brome_bacfunarc_dw_otu_table-graph_centrality-degree-selectallbyall-abundances.csv")
+test_AUC = pd.read_csv("./test_data/brome.dg.auc.csv")
+main_dataFrame( test_AUC, test_abundance, test_sample, "Test_Step6", True, True)
 
 # test_sample = "./test_data/test_sample.csv"
 # test_abundance = pd.read_csv("./test_data/bromeA_all-abundances-0.csv")
