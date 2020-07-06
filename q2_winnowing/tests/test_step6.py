b2ea7825705cd356a31eb9f6d065d30e3cf23edd
ldb385 <ldb385@usask.ca>
2020-07-06 13:31:25 -0600

    step6 currently tested will add more test data

--- q2_winnowing/tests/test_step6.py
+++ q2_winnowing/tests/test_step6.py
@@ -1,9 +1,11 @@
 
 from unittest import TestCase, main as unittest_main
 import pandas as pd
+import numpy as np
 import os
 
 from q2_winnowing.step6.Step6_Permanova import main as step6_main
+from q2_winnowing.step6.Step6_Permanova import _convert_to_dist_hel_matrix as array_to_hel
 
 class Step6Tests( TestCase ):
 
@@ -13,6 +15,18 @@ class Step6Tests( TestCase ):
     samples_df_in = pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step6/test_in_samples.csv")
     # <><> read output values for comparison <><>
     permanova_df_out = pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step6/test_out_PERMANOVA.csv", index_col=0 )
+
+    def test_step6_convert_linear_to_hel(self):
+
+        test_array_in = [1,2,3,4,5,6]
+        test_hel_out = [[0, 1, 2, 3],
+                        [1, 0, 4, 5],
+                        [2, 4, 0, 6],
+                        [3, 5, 6, 0]]
+        array_to_hel_output = array_to_hel( test_array_in, 4 )
+
+        np.testing.assert_array_equal( array_to_hel_output, test_hel_out )
+
 
     def test_step6_main(self):
 
