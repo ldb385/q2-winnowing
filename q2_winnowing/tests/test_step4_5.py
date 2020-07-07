e0c588c38f805a60a2344d8da2176957fdd0ba73
ldb385 <ldb385@usask.ca>
2020-07-07 13:37:32 -0600

    step6 currently tested will add more test data

--- q2_winnowing/tests/test_step4_5.py
+++ q2_winnowing/tests/test_step4_5.py
@@ -2,6 +2,7 @@
 from unittest import TestCase, main as unittest_main
 import os
 import pandas as pd
+import numpy as np
 
 from q2_winnowing.step4_5.Step4and5_DecayCurve import main as step4_5_main
 
@@ -11,23 +12,23 @@ class Step4_5Tests( TestCase ):
     testing_data = []
     testing_data.append((
         pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIC_0.2_dw_graph_centrality_dg.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_dg_param.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_dg_auc.csv")
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_dg_param.csv", index_col=0 ),
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_dg_auc.csv", index_col=0 )
     ))
     testing_data.append((
         pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIN_3_dw_graph_centrality_cl.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_cl_param.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_cl_auc.csv")
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_cl_param.csv", index_col=0 ),
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_cl_auc.csv", index_col=0 )
     ))
     testing_data.append((
         pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIN_3_dw_graph_centrality_ei.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_ei_param.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_ei_auc.csv")
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_ei_param.csv", index_col=0 ),
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_ei_auc.csv", index_col=0 )
     ))
     testing_data.append((
         pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIN_3_MIC_0.2_dw_graph_centrality_bw.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_bw_param.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_bw_auc.csv")
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_bw_param.csv", index_col=0 ),
+        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_bw_auc.csv", index_col=0 )
     ))
 
 
@@ -36,13 +37,15 @@ class Step4_5Tests( TestCase ):
         for important_features_in, parameter_out, auc_out in self.testing_data:
             auc_result, auc_param = step4_5_main( important_features_in, "", False, False )
 
-            self.assertEqual(
-                auc_result.to_numpy(),
-                auc_out.values.to_numpy()
+            np.testing.assert_array_almost_equal(
+                auc_result.to_numpy().astype(type(0.0)),
+                auc_out.to_numpy().astype(type(0.0)),
+                decimal=15
             )
-            self.assertEqual(
-                auc_param.values.to_numpy(),
-                parameter_out.values.to_numpy()
+            np.testing.assert_array_almost_equal(
+                auc_param.to_numpy().astype(type(0.0)),
+                parameter_out.to_numpy().astype(type(0.0)),
+                decimal=15
             )
 
 
