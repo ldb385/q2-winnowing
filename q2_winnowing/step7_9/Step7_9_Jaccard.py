3273c1d168419e9941b43f0a18ebe2568ec41a2f
ldb385 <ldb385@usask.ca>
2020-07-08 09:29:56 -0600

    index was starting at 1 reset to start at 0 for consistency with other file output

--- q2_winnowing/step7_9/Step7_9_Jaccard.py
+++ q2_winnowing/step7_9/Step7_9_Jaccard.py
@@ -102,6 +102,7 @@ def main( df_leaveOneOut, name, detailed=False, verbose=False ):
 
         kappa_df.loc[i + 1] = [_kConditioning, _kCentrality, _kCorrel, _kThreshold, _kSelect_iter, _kKappa, _kAgreement]
 
+    kappa_df.reset_index( drop=True, inplace=True ) # index should start at 0 for consistency
 
     if( detailed ):
         outFile = f"{outDir}/{name}_Jaccard_result.csv"
