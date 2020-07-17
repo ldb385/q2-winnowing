b0ec411bd3f6fe0bd8e3c31e511bba1035ac93bb
ldb385 <ldb385@usask.ca>
2020-07-17 11:51:10 -0600

    table is visualized Woot Woot. Def needs css though

--- q2_winnowing/_summarize/_visualizer.py
+++ q2_winnowing/_summarize/_visualizer.py
@@ -1,5 +1,6 @@
 
 import pandas as pd
+import shutil
 import os
 import pkg_resources
 import qiime2
@@ -12,18 +13,15 @@ def summarize( output_dir: str, data: list ) -> None:
     featureOrdering, AucOrdering, PermanovaOrdering = data[0] # Split directory into its parts
 
     # <><><> Allow user to download data <><><>
-    # save out feature ordering data for download
-    featureOrdering.index.name = 'id'
-    featureOrdering = qiime2.Metadata( featureOrdering.to_frame())
-    featureOrdering.save(os.path.join( output_dir, 'feature_ordered.tsv')) # download
-    # save out AUC ordering data for download
-    AucOrdering.index.name = 'id'
-    AucOrdering = qiime2.Metadata( AucOrdering.to_frame())
-    AucOrdering.save(os.path.join( output_dir, 'auc_ordered.tsv')) # download
-    # save out PERMANOVA ordering data for download
-    PermanovaOrdering.index.name = 'id'
-    PermanovaOrdering = qiime2.Metadata( PermanovaOrdering.to_frame())
-    PermanovaOrdering.save(os.path.join( output_dir, 'permanova_ordered.tsv')) # download
+    # # save out feature ordering data for download
+    # featureOrdering = qiime2.Metadata( "FO" )
+    # featureOrdering.save(os.path.join( output_dir, 'feature_ordered.tsv')) # download
+    # # save out AUC ordering data for download
+    # AucOrdering = qiime2.Metadata( "AO")
+    # AucOrdering.save(os.path.join( output_dir, 'auc_ordered.tsv')) # download
+    # # save out PERMANOVA ordering data for download
+    # PermanovaOrdering = qiime2.Metadata( "PO")
+    # PermanovaOrdering.save(os.path.join( output_dir, 'permanova_ordered.tsv')) # download
 
     # Get html files that will be written to
     index_html = os.path.join( TEMPLATES, "assets", "index.html" )
@@ -31,10 +29,20 @@ def summarize( output_dir: str, data: list ) -> None:
     auc_html = os.path.join( TEMPLATES, "assets", "auc.html" )
     permanova_html = os.path.join( TEMPLATES, "assets", "permanova.html" )
 
+    # Write files to output directory
+    index_html_new = os.path.join( output_dir, "index.html" )
+    shutil.copyfile( index_html, index_html_new )
+    featureOrdering_html_new = os.path.join( output_dir, "featureOrdering.html" )
+    shutil.copyfile( featureOrdering_html, featureOrdering_html_new )
+    auc_html_new = os.path.join( output_dir, "auc.html" )
+    shutil.copyfile( auc_html, auc_html_new )
+    permanova_html_new = os.path.join( output_dir, "permanova.html" )
+    shutil.copyfile( permanova_html, permanova_html_new )
+
     # Write tables to specific html files
-    featureOrdering.to_html( open( featureOrdering_html, "w" ))
-    AucOrdering.to_html( open( auc_html, "w" ))
-    PermanovaOrdering.to_html( open( permanova_html, "w" ))
+    featureOrdering.to_html( open( featureOrdering_html_new, "w" ))
+    AucOrdering.to_html( open( auc_html_new, "w" ))
+    PermanovaOrdering.to_html( open( permanova_html_new, "w" ))
 
 
-    return
+    return # Standard to not return on qiime2 visualizer
