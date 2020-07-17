ec76ac010669cbfccc654e6cf8d3b6c0ef4295b2
ldb385 <ldb385@usask.ca>
2020-07-17 16:36:57 -0600

    created for documentation of what is left to do in the plugin before publishing

---
+++ TODO.py
@@ -0,0 +1,11 @@
+
+# This will serve as a reminder of things to complete as plugin approaches final stages
+#TODO: Verify correct parameters
+#TODO: Finish testing of Step 1 - 3
+#TODO: Get review from user group
+#TODO: Go through code and check for optimization and error handling
+#TODO: Run through possible inputs to plugin
+#TODO: Do a fresh installation of plugin and record process for easier user installation
+
+
+
