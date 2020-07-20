b8690a48abd9e4f5bc586dc8e448f0c7cf5bd33a
ldb385 <ldb385@usask.ca>
2020-07-20 11:54:27 -0600

    read paths as needed not all at once

--- q2_winnowing/tests/test_step1_3.py
+++ q2_winnowing/tests/test_step1_3.py
@@ -13,12 +13,25 @@ class Step1_3Tests( TestCase ):
     # <><> read input values and expected output values for testing <><>
     testing_data = []
     testing_data.append((
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_data.csv"),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_features.csv",
-                    index_col=0),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_important.csv",
-                    index_col=0),
-        pd.read_csv(f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_abundances.csv",
-                    index_col=0),
+        f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step1_3/test_in_data.csv",
+        f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step1_3/test_out_features.csv",
+        f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step1_3/test_out_important.csv",
+        f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step1_3/test_out_abundances.csv",
     ))
+
+    def test_step1_3_main(self):
+
+        for data_path, features_path, important_path, adundances_path in self.testing_data:
+            data_in = pd.read_csv( data_path )
+            features_out = pd.read_csv( features_path, index_col=0 )
+            important_out = pd.read_csv( important_path, index_col=0 )
+            adundances_out = pd.read_csv( adundances_path, index_col=0 )
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest_main()
 
