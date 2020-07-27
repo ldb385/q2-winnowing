a7c8265e2b33f57f554c1bb899840fba236ffc4a
ldb385 <ldb385@usask.ca>
2020-07-27 11:34:17 -0600

    split up a test case that encapsulated many tests. this will make testing more transparent

--- q2_winnowing/tests/test_step4_5.py
+++ q2_winnowing/tests/test_step4_5.py
@@ -12,50 +12,108 @@ class Step4_5Tests( TestCase ):
     package = 'q2_winnowing.tests'
 
     # <><> read input values and expected output values for testing <><>
-    testing_data = []
-    testing_data.append((
+    testing_data = {
+        "degree": (
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIC_0.2_dw_graph_centrality_dg.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_dg_param.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_dg_auc.csv"
-    ))
-    testing_data.append((
+        ),
+        "closeness": (
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIN_3_dw_graph_centrality_cl.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_cl_param.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_cl_auc.csv"
-    ))
-    testing_data.append((
+        ),
+        "eigenvector": (
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIN_3_dw_graph_centrality_ei.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_ei_param.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_ei_auc.csv"
-    ))
-    testing_data.append((
+        ),
+        "betweeness": (
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_in_MIN_3_MIC_0.2_dw_graph_centrality_bw.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_bw_param.csv",
         f"{os.path.dirname(os.path.realpath(__file__))}/sample_data/step4_5/test_out_bw_auc.csv"
-    ))
+        )
+    }
 
 
-    def test_step4_5_main(self):
+    def test_step4_5_main_degree(self):
 
-        for important_features_path, parameter_path, auc_path in self.testing_data:
-            # Read in data as needed to avoid memory consumption
-            important_features_in = pd.read_csv( important_features_path )
-            auc_result, auc_param = step4_5_main( important_features_in, "", False, False )
+        important_features_path, parameter_path, auc_path = self.testing_data["degree"]
+        important_features_in = pd.read_csv( important_features_path )
+        auc_result, auc_param = step4_5_main( important_features_in, "", False, False )
+        parameter_out = pd.read_csv( parameter_path, index_col=0 )
+        auc_out = pd.read_csv( auc_path, index_col=0 )
+        # Read in data as needed to avoid unnecessary memory consumption
 
-            # Read in data as needed to avoid memory consumption
-            important_features_in = None
-            parameter_out = pd.read_csv( parameter_path, index_col=0 )
-            auc_out = pd.read_csv( auc_path, index_col=0 )
+        np.testing.assert_array_equal(
+            auc_result.to_numpy().astype(object),
+            auc_out.to_numpy().astype(object),
+        )
+        np.testing.assert_array_almost_equal(
+            auc_param.to_numpy().astype(type(0.0)),
+            parameter_out.to_numpy().astype(type(0.0)),
+            decimal=15
+        )
 
-            np.testing.assert_array_equal(
-                auc_result.to_numpy().astype(object),
-                auc_out.to_numpy().astype(object),
-            )
-            np.testing.assert_array_almost_equal(
-                auc_param.to_numpy().astype(type(0.0)),
-                parameter_out.to_numpy().astype(type(0.0)),
-                decimal=15
-            )
+
+    def test_step4_5_main_closeness(self):
+
+        important_features_path, parameter_path, auc_path = self.testing_data["closeness"]
+        important_features_in = pd.read_csv( important_features_path )
+        auc_result, auc_param = step4_5_main( important_features_in, "", False, False )
+        parameter_out = pd.read_csv( parameter_path, index_col=0 )
+        auc_out = pd.read_csv( auc_path, index_col=0 )
+        # Read in data as needed to avoid unnecessary memory consumption
+
+        np.testing.assert_array_equal(
+            auc_result.to_numpy().astype(object),
+            auc_out.to_numpy().astype(object),
+        )
+        np.testing.assert_array_almost_equal(
+            auc_param.to_numpy().astype(type(0.0)),
+            parameter_out.to_numpy().astype(type(0.0)),
+            decimal=15
+        )
+
+
+    def test_step4_5_main_eigenvector(self):
+
+        important_features_path, parameter_path, auc_path = self.testing_data["eigenvector"]
+        important_features_in = pd.read_csv( important_features_path )
+        auc_result, auc_param = step4_5_main( important_features_in, "", False, False )
+        parameter_out = pd.read_csv( parameter_path, index_col=0 )
+        auc_out = pd.read_csv( auc_path, index_col=0 )
+        # Read in data as needed to avoid unnecessary memory consumption
+
+        np.testing.assert_array_equal(
+            auc_result.to_numpy().astype(object),
+            auc_out.to_numpy().astype(object),
+        )
+        np.testing.assert_array_almost_equal(
+            auc_param.to_numpy().astype(type(0.0)),
+            parameter_out.to_numpy().astype(type(0.0)),
+            decimal=15
+        )
+
+
+    def test_step4_5_main_betweeness(self):
+
+        important_features_path, parameter_path, auc_path = self.testing_data["betweeness"]
+        important_features_in = pd.read_csv( important_features_path )
+        auc_result, auc_param = step4_5_main( important_features_in, "", False, False )
+        parameter_out = pd.read_csv( parameter_path, index_col=0 )
+        auc_out = pd.read_csv( auc_path, index_col=0 )
+        # Read in data as needed to avoid unnecessary memory consumption
+
+        np.testing.assert_array_equal(
+            auc_result.to_numpy().astype(object),
+            auc_out.to_numpy().astype(object),
+        )
+        np.testing.assert_array_almost_equal(
+            auc_param.to_numpy().astype(type(0.0)),
+            parameter_out.to_numpy().astype(type(0.0)),
+            decimal=15
+        )
 
 
 
