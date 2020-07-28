2f6012c94b1032e1d4f95d7a945509e7c2b5fb3b
ldb385 <ldb385@usask.ca>
2020-07-28 10:11:45 -0600

    spelling fix

--- q2_winnowing/_summarize/_visualizer.py
+++ q2_winnowing/_summarize/_visualizer.py
@@ -59,7 +59,7 @@ def summarize( output_dir: str, data: list ) -> None:
             {table}
         </body>
     </html>
-    """ # this will like iframes to css stylesheets in order to format tables nicer then default in pandas.to_html
+    """ # this will link iframe to css stylesheets in order to format tables nicer then default in pandas.to_html
     with open( feature_ordering_html_new, "w" ) as f:
         f.write( dataframe_format_string.format( table=feature_ordering.to_html(classes="style_df")) )
     with open( auc_html_new, "w" ) as f:
