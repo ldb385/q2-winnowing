e64893a19f11ee0033d06b436b4e1cb5e66c8b44
ldb385 <ldb385@usask.ca>
2020-08-25 14:17:03 -0600

    Bug: Removed unnecessary imports. Test all still pass

--- q2_winnowing/step6/permanova.py
+++ q2_winnowing/step6/permanova.py
@@ -9,11 +9,8 @@ import math
 from rpy2.robjects.packages import importr
 import rpy2.robjects.packages as rpackages
 from rpy2.robjects.vectors import StrVector
-from rpy2.robjects import r
-from scipy.spatial.distance import pdist,squareform
 from rpy2.robjects import pandas2ri
 from rpy2.robjects import Formula
-import rpy2.robjects as robjects
 import matplotlib.pyplot as plt
 from scipy.interpolate import UnivariateSpline
 
@@ -23,7 +20,7 @@ pandas2ri.activate()
 utils = rpackages.importr('utils')
 utils.chooseCRANmirror(ind=1)
 
-packnames = ('vegan', 'scales','data.table','zoo')
+packnames = ['vegan','zoo','stats']
 names_to_install = []
 #names_to_install = [x for packnames if not rpackages.isinstalled(x)]
 
@@ -36,14 +33,8 @@ if len(names_to_install) > 0:
 
 # Setup R packages
 rvegan = importr('vegan')
-rscales = importr('scales')
 rzoo = importr('zoo')
-rdatatable = importr('data.table')
 rstats = importr('stats')
-rprint = r['print']
-rpar = r['par']
-rplot = r['plot']
-
 
 # <><><> DEFINE FUNCTIONS <><><>
 
