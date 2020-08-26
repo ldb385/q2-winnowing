9b36572c106920f04016a37955fb2665d3ad84d2
ldb385 <ldb385@usask.ca>
2020-08-26 14:31:53 -0600

    Build: All citations displayed by plugin

--- q2_winnowing/plugin_setup.py
+++ q2_winnowing/plugin_setup.py
@@ -12,8 +12,6 @@ from ._format import ( WinnowedDirectoryFormat, WinnowedFeatureOrderingFormat,
 from q2_winnowing.winnow import process
 from q2_winnowing._summarize._visualizer import summarize
 
-cites = qiime2.plugin.Citations.load("citations.bib", package="q2_winnowing")
-
 # Setup Choice lists for easy accessibility
 # <><><> NOTE: THESE ARE FOR STEPS 1-3 <><><>
 _METRIC_TYPES_ = ["graph_centrality", "pca_importance", "log_transform"]
@@ -32,7 +30,7 @@ plugin = qiime2.plugin.Plugin(
     version=q2_winnowing.__version__,
     website="https://github.com/ldb385/q2-winnowing",
     package="q2_winnowing",
-    citations=[cites],
+    citations=qiime2.plugin.Citations.load("citations.bib", package="q2_winnowing"),
     description="A Qiime2 plugin used to perform a feature selection on data in order to generate a winnowed community."
                 " Diversity and Environmental measures can then be performed on this output to measure the connectivity"
                 " of the generated winnowed community.",
